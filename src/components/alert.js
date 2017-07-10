7390e3420a92d9da9e7e955d74aeb0b254c1e02b
Craig Frost <c.frostwr1@gmail.com>
2017-07-10 17:17:01 +0100

    Update Alert component

--- src/components/alert.js
+++ src/components/alert.js
@@ -2,57 +2,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import Layout from './layout';
+import Text from './text';
 import Icon from './icon';
+
 import { opacityHoverEffect } from '../transitions';
-import { getVariant, hexToRgba } from '../utils';
-import { black, grey, white } from '../theme';
+import { getVariant, hexToRgba, pxToRem } from '../utils';
+import { white } from '../theme';
 
-const Container = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  padding: 1em;
-  color: ${props => getVariant(props) || 'inherit'};
-  background-color: #fff;
-  border-radius: 3px;
-  border: 1px solid ${grey};
-  border-color: ${props => hexToRgba(getVariant(props) || grey, 0.8)};
-  box-shadow: 0 2px 5px ${props => hexToRgba(getVariant(props) || black, 0.1)};
-  ${props =>
-    props.filled &&
-    `
-    background-color: ${hexToRgba(getVariant(props) || white, 0.1)};
-  `}
-
-  > h4 {
-    font-weight: 500;
-    margin: 0 0 .5em;
-  }
-
-  > p {
-    margin: 0;
-  }
+const Container = styled(Layout).attrs({
+  justified: true,
+  center: true,
+  gutter: 12,
+})`
+  padding: ${pxToRem(12)} ${pxToRem(18)};
+  background-color: ${props => hexToRgba(getVariant(props) || white, 0.15)};
+  border-radius: 2px;
 `;
 
-const DismissIcon = styled(Icon)`
-  position: absolute;
-  top: 5px;
-  right: 5px;
-  color: inherit;
+const DismissIcon = styled(Icon).attrs({
+  color: 'inherit',
+  name: 'close',
+  size: 18,
+})`
   ${opacityHoverEffect};
 `;
 
 class Alert extends Component {
   static propTypes = {
-    title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
     dismiss: PropTypes.bool,
-    filled: PropTypes.bool,
   };
 
   static defaultProps = {
     dismiss: false,
-    filled: false,
   };
 
   state = {
@@ -68,11 +51,12 @@ class Alert extends Component {
       return null;
     }
 
-    const { children, dismiss, title, ...other } = this.props;
+    const { children, dismiss, ...other } = this.props;
     return (
       <Container {...other}>
-        {title && <h4>{title}</h4>}
-        {children}
+        <Text>
+          {children}
+        </Text>
         {dismiss &&
           <DismissIcon name="close" size="16" onClick={this.dismiss} />}
       </Container>
